9977587b0de2eec63a0165567d3d1a7a9140a440
sonhodajade <marcosdelamora422@gmail.com>
2026-09-09 21:33:47 -0300

    divisão

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+#include<locale.h>
 
 int main(){
 
+    Setlocale(LC_ALL, "pt_BR.UTF-8");
+    
     int a = 5;
     int b = 4;
 
-    printf("O maior número é:%d\n", (a + b));
+    printf("a soma é:%d\n", (a + b));
 
     printf("a multiplicação é:%d\n", (a * b));
 
