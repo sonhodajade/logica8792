5c68d9167143ec5aee00ea0a8ba8af8fe7bb01b3
sonhodajade <marcosdelamora422@gmail.com>
2026-09-14 21:49:24 -0300

    diferentes\iguais

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ setlocale(LC_ALL, "pt_BR.UTF-8");
     printf("Digite dois números: ");
     scanf("%d %d", &a, &b);
 
-    if(a == b){
+    if(a != b){
         printf("Os números são iguais!\n");
     }else{
         printf("Os números são diferentes!\n");
