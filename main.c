9041b14db88b56fb14f5167e058d139775b0b3ef
sonhodajade <marcosdelamora422@gmail.com>
2026-09-14 21:18:05 -0300

    menor

--- main.c
+++ main.c
@@ -3,20 +3,37 @@
 
 int main(){
 
-    Setlocale(LC_ALL, "pt_BR.UTF-8");
+setlocale(LC_ALL, "pt_BR.UTF-8");
     
-    
-    int n;
 
-printf("Digite um idade: ");
-scanf("%d", &idade); 
+    int a, b, c, d, e, menor;
 
-if(idade >=18) {
-    printf("Você é maior de idade!");
-}else{
-    printf("você é menor de idade!");
+
+    printf("Digite um número para a: ");
+    scanf("%d", &a);
+
+    printf("Digite um número para b: ");
+    scanf("%d", &b);
+
+    printf("Digite um número para c: "); 
+    scanf("%d", &c);
+
+    printf("Digite um número para d: ");
+    scanf("%d", &d);
+
+    printf("Digite um número para e: ");
+    scanf("%d", &e);
+
+
+    menor = a;
+
+    if(b < menor) menor = b;
+    if(c < menor) menor = c;
+    if(d < menor) menor = d;
+    if(e < menor) menor = e;
+
+    printf("Menor:%d", menor); 
+
+     return 0;
 }
 
-
-    return 0;
-}
