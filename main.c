e7122a8e0e90b74ba457902ec920497c2b2c0673
sonhodajade <marcosdelamora422@gmail.com>
2026-09-16 19:54:05 -0300

    media, freq

--- main.c
+++ main.c
@@ -6,19 +6,30 @@ int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
     
 
-    int idade;
-    int ehEstudante;
+    int n1, n2, n3;
+    float media;
+    float freq;
 
-    printf("Qual sua idade?");
-    scanf("%d", &idade);
-    printf("\nVocê é estudante (0 - Não | 1 - Sim):");
-    scanf("%d", ehEstudante); 
+    printf("digite a nota 1: ");
+    scanf("%d", &n1);
 
-    if((idade >= 60) && (ehEstudante == 1)){
-        printf("Parabéns, você tem direito a desconto!");
+    printf("\nDigite nota 2: ");
+    scanf("%d", &n2);
+
+    printf("\nDigite nota 3: ");
+    scanf("%d", &n3);
+
+    media = (n1 + n2 + n3) / 3;
+
+    printf("Digite a frequência (em números apenas): ");
+    scanf("%f", &freq);
+
+    if(media >= 7 && freq >= 75){
+        printf("\nAprovado com sucesso!");
     }else{
-        printf("Infelizmente você não terá desconto!");
+        printf("\nReprovado!");
     }
+    
      return 0;
 }
 
