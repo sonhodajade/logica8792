2c5fbb263fe12659765bffbc3fb14d71fbe31570
sonhodajade <marcosdelamora422@gmail.com>
2026-09-16 19:24:28 -0300

    roleta da morte

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ setlocale(LC_ALL, "pt_BR.UTF-8");
     printf("\nQual sua altura?");
     scanf("%f", &altura);
 
-    if((idade >= 18) && (altura >= 1.60)){
+    if((idade >= 18) || (altura >= 1.60)){
         printf("Você pode brincar na roleta!");
     }else{
         printf("Você não pode brincar na roleta!");
