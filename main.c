c4a6d8cae391e7a2919d4ca11fcbb3dd12e3a8d7
sonhodajade <marcosdelamora422@gmail.com>
2026-09-16 20:50:08 -0300

    diaDasemana

--- main.c
+++ main.c
@@ -8,25 +8,41 @@ setlocale(LC_ALL, "pt_BR.UTF-8");
 
     int diaDasemana;
 
-    printf("Digite um número de (1 a 7): ");
-    scanf("%d", &diaDasemana);
+    printf("Digite um número de 1 a 7
+    scanf("%d", &dia);
 
-    if(diaDasemana == 1){
-        printf("Domingo!");
-    }else if(diaDasemana == 2){
-        printf("Segunda-feira");
-    }else if(diaDasemana == 3){
-        printf("Terça-feira");
-    }else if(diaDasemana == 4){
-        printf("Quarta-feira");
-    }else if(diaDasemana == 5){
-        printf("Quinta-feira");
-    }else if(diaDasemana == 6){
-        printf("Sexta-feira");
-    }else if(diaDasemana == 7){
-        printf("Sábado!");
-    }else{
-        printf("valor inválido!");
+    switch(dia){
+    case 1:
+    printf("Domingo!");
+    break;
+
+    case 2:
+    printf("Segunda-feira");
+    break;
+
+    case 3:
+    printf("terça-feira");
+    break;
+
+    case 4:
+    printf("Quarta-feira");
+    break;
+
+    case 5:
+    printf("Quinta-feira");
+    break;
+
+    case 6:
+    printf("Sexta-feira");
+    break;
+
+    case 7:
+    printf("Sábado!");
+    break;
+
+    default:
+    printf("Número inválido!!");
+    break;
     }
     
      return 0;
