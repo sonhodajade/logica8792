c1dbce2b9a5bff4a8e4c4a4f24b5976c860b63f3
sonhodajade <marcosdelamora422@gmail.com>
2026-09-22 20:58:46 -0300

    funções

--- main.c
+++ main.c
@@ -1,35 +1,7 @@
 #include<stdio.h>
 #include<locale.h>
 
-int somar(int a, int b){
-    return a + b;
-}
 
-int subtrair(int a, int b){
-    return a - b;
-}
-
-int multiplicar(int a, int b){
-    return a * b;
-}
-
-int dividir(int a, int b){
-    if(b <=0){
-        printf("Ele precisa ser maior que 0!");
-    }else{
-        return a / b;
-    }
-}
-
-// // void soma(){
-// //     int num1, num2;
-// //     printf("Digite um valor para num1: ");
-// //     scanf("%d", &num1);
-// //     printf("Digite um valor para num2: ");
-// //     scanf("%d", &num2);
-// //     int resultado = num1 + num2;
-// //     printf("O resultado da soma com void é %d\n", resultado 1 );
-// }
 
 
 
