239fc77f2c57ef5f56aa24609c628a0b66dcf892
sonhodajade <marcosdelamora422@gmail.com>
2026-09-22 20:10:23 -0300

    resultao

--- main.c
+++ main.c
@@ -5,13 +5,27 @@ int somar(int a, int b){
     return a + b;
 }
 
+// // void soma(){
+// //     int num1, num2;
+// //     printf("Digite um valor para num1: ");
+// //     scanf("%d", &num1);
+// //     printf("Digite um valor para num2: ");
+// //     scanf("%d", &num2);
+// //     int resultado = num1 + num2;
+// //     printf("O resultado da soma com void é %d\n", resultado 1 );
+// }
+
+
+
 int main(){
     setlocale(LC_ALL, "pt_br.UTF-8");
 
-    saudar("Marcos");
-    saudar("Silva");
-    saudar("Ferreira");
+    int resultado = somar (5, 3);
+    printf("o resultado de soma é: %d", resultado);
 
+    
+
+   
 
 
 
