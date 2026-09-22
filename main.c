125641bd1f2aa4ca4b0364cbc4eb51f69184201c
sonhodajade <marcosdelamora422@gmail.com>
2026-09-22 19:25:09 -0300

    função

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 #include<locale.h>
 
+void dobrar (int *x){
+    *X = *X * 2;
+}
+
 int main(){
 
 setlocale(LC_ALL, "pt_BR.UTF-8");
+
+int numero = 10
+dobrar(&numero);
+printf("Número depois da função %d\n", numero);
 
 int x = 10;
 int *p = &x;
