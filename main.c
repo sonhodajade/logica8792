d770e76150d3d70aa00f6ff1dce9568914c88e77
sonhodajade <marcosdelamora422@gmail.com>
2026-09-23 19:59:17 -0300

    tabuada

--- main.c
+++ main.c
@@ -8,23 +8,19 @@
 
 
 
-void fibonacci(int termos){
-    int a = 0, b = 1, c;
-    printf("Sequência de Fibonacci (%d termos): \n", termos);
-    for (int i = 1; i <= termos; i++){
-        printf("%d\n", a);
-        c = a + b;
-        a = b;
-        b = c;
+void tabuada(int n){
+    printf("Tabuada do %d\n", n);
+    for(int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", n, i, n * i);
     }
-    printf("\n");
 }
+
 
 
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
    
-    fibonacci(10);
+   tabuada(7);
 
      return 0;
 
