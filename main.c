97c30bd1f206981b80810ef1e82fa6ebd0ba106c
sonhodajade <marcosdelamora422@gmail.com>
2026-09-22 21:21:11 -0300

    funções

--- main.c
+++ main.c
@@ -10,15 +10,22 @@
 int main(){
     setlocale(LC_ALL, "pt_br.UTF-8");
 
-    int num[5] = {10, 20, 30, 40, 50};
+    int horas[12];
+    int minutos[60];
 
-    for(int i = 0; i < 5; i++){
-        printf("%d\n", num[i]);
-        
+    for(int i = 0; i < 12; i++){
+        horas[i] = i + 1;
+    }
+    for(int j = 0; j < 60; j++){
+        minutos[j] = j;
     }
     
-    
-
+    // simula o relogio
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 60; J++){
+            printf("Hora: %2d:%02d\n", horas[i], minutos[j]);
+        }
+    }
 
 
     
