73dd2471c8ef46a16bd5cf89dde2be28138ca8cf
sonhodajade <marcosdelamora422@gmail.com>
2026-09-23 19:46:43 -0300

    fibonacci

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 
 void fibonacci(int termos){
-    int a = 0; b = 1; c;
+    int a = 0, b = 1, c;
     printf("Sequência de Fibonacci (%d termos): \n", termos);
     for (int i = 1; i <= termos; i++){
         printf("%d\n", a);
