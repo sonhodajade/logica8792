9a7aacbe1f4e2fa28787ebca9a58878564024e22
sonhodajade <marcosdelamora422@gmail.com>
2026-09-22 21:45:52 -0300

    potência

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<locale.h>
+#include<math.h>
 
 
 
@@ -10,27 +11,15 @@
 int main(){
     setlocale(LC_ALL, "pt_br.UTF-8");
 
-    char nomes[3][20] ={
-        "Elisandro",
-        "fahur",
-        "Maria Helena"
-    };
-
-    for(int i = 0; i < 3; i++){
-        printf("%s\n", nomes[i]);
-    }
-
-
+    int numero;
+    printf("Digite um número: ");
+    scanf("%d, &numero); ")
     
+    int quadrado = (int)pow(numero, 2);
+    int cubo = (int)pow(numero, 3);
+    printf("Quadrado de %d = %d\n", numero, quadrado);
+    printf("Cubo de %d = %d", numero, cubo);
 
-   
-
-
-
-
-
- 
-    
      return 0;
 }
 
