a993fd98f47bf3f9c4071e493a7b5c8897c011ad
sonhodajade <marcosdelamora422@gmail.com>
2026-09-22 21:37:01 -0300

    for nomes

--- main.c
+++ main.c
@@ -10,21 +10,14 @@
 int main(){
     setlocale(LC_ALL, "pt_br.UTF-8");
 
-    int horas[12];
-    int minutos[60];
+    char nomes[3][20] ={
+        "Elon Musk"
+        "fahur"
+        "Maria Helena"
+    };
 
-    for(int i = 0; i < 12; i++){
-        horas[i] = i + 1;
-    }
-    for(int j = 0; j < 60; j++){
-        minutos[j] = j;
-    }
-    
-    // simula o relogio
-    for(int i = 0; i < 12; i++){
-        for(int j = 0; j < 60; j++){
-            printf("Hora: %2d:%02d\n", horas[i], minutos[j]);
-        }
+    for(int i = 0; i < 3; f++){
+        printf("%s\n", nomes[i]);
     }
 
 
