70fdfdfa17dca1513f3057eb57278288f1565113
sonhodajade <marcosdelamora422@gmail.com>
2026-09-24 20:51:39 -0300

    soma for de numero ate n

--- main.c
+++ main.c
@@ -10,14 +10,16 @@
 int main(){
 setlocale(LC_ALL, "pt_br.UTF-8");
 
-int i = 1;
-while(i <= 50){
-    if(i % 2 == 0){
-        printf("%d\n", i);
-    }
-    i++;
+int n, soma = 0;
+
+printf("Digite um valor para n: ");
+scanf("%d", &n);
+
+for(int i = 1; i <= n; i++){
+    soma +=i;
 }
 
+printf("O resultado da soma é: %d\n", soma);
 
 
 
