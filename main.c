0e1917dc1d96874c695fbc30c567f3703a5d4e97
sonhodajade <marcosdelamora422@gmail.com>
2026-09-24 19:40:35 -0300

    while + switch

--- main.c
+++ main.c
@@ -9,16 +9,25 @@ int main(){
 setlocale(LC_ALL, "pt_br.UTF-8");
 
 
-int numero;
-printf("Digite um número positivo: ");
-scanf("%d", &numero);
+int opcao = -1;
 
-while(numero <= 0){
+while (opcao !=){
+    printf("\nMenu: \n");
+    printf("1 - Depositar\n");
+    printf("2 - Sacar\n");
+    printf("3 - Consultar\n");
+    printf("0 - sair\n");
+    printf("Escolha: ");
+    scanf("%d, &opcao");
 
-printf("Número inválido! Digite novamente: ");
-scanf("%d", &numero);
+    switch(opcao){
+        case 1: printf("você escolheu depositar\n"); break;
+        case 2: printf("Você escolheu sacar\n"); break;
+        case 3: printf("Você escolheu consultar\n"); break;
+        case 0: printf("saindo..."); break;
+        default: printf("opção invalida!\n");
+    }
 }
-printf("Número válido: %d\n", numero);
 
    
 
