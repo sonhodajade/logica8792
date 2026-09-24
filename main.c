57986aa31d4891daa9b40bcfdaeb0e254e176a27
sonhodajade <marcosdelamora422@gmail.com>
2026-09-24 19:20:28 -0300

    int numero

--- main.c
+++ main.c
@@ -5,19 +5,16 @@
 
 
 
-
-
-
-
 int main(){
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+setlocale(LC_ALL, "pt_br.UTF-8");
 
-    int i = 4;
 
-    while(i <= 5){
-        printf("%d\n", i);
-        i++;
-    }
+int numero;
+printf("Digite um número positivo: ");
+scanf("%d", &numero <= 0){
+printf("Númeor inválido! Digite novamente: ");
+scanf("%d", &numero);
+}printf("Número válido: %d\n", numero);
    
 
      return 0;
