b5d3522ac7713fe32d116708aee1f80d7560554f
sonhodajade <marcosdelamora422@gmail.com>
2026-09-24 20:36:50 -0300

    for winter edu!

--- main.c
+++ main.c
@@ -10,8 +10,12 @@
 int main(){
 setlocale(LC_ALL, "pt_br.UTF-8");
 
-for(int i = 0; i <= 50; i += 2){
-    printf("%d\n", i);
+int i = 1;
+while(i <= 50){
+    if(i % 2 == 0){
+        printf("%d\n", i);
+    }
+    i++;
 }
 
 
