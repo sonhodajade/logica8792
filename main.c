35f0a7411a33dc7e37246db2d443984a2fd0e69d
sonhodajade <marcosdelamora422@gmail.com>
2026-09-24 20:15:27 -0300

    numero somar

--- main.c
+++ main.c
@@ -9,25 +9,16 @@ int main(){
 setlocale(LC_ALL, "pt_br.UTF-8");
 
 
-int opcao = -1;
+int numero, soma = 0;
 
-while (opcao != 0){
-    printf("\nMenu: \n");
-    printf("1 - Depositar\n");
-    printf("2 - Sacar\n");
-    printf("3 - Consultar\n");
-    printf("0 - sair\n");
-    printf("Escolha: ");
-    scanf("%d", &opcao);
+printf("digite número para somar (0 encerra): ");
+scanf("%d", &numero);
 
-    switch(opcao){
-        case 1: printf("você escolheu depositar\n"); break;
-        case 2: printf("Você escolheu sacar\n"); break;
-        case 3: printf("Você escolheu consultar\n"); break;
-        case 0: printf("saindo..."); break;
-        default: printf("opção invalida!\n");
-    }
+while(numero !=0){
+    soma += numero;
+    scanf("%d", &numero);
 }
+printf("Soma total = %d\n", soma);
 
    
 
