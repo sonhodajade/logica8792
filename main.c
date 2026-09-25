d0e471effea09cdd7867664aad079ff10018a912
sonhodajade <marcosdelamora422@gmail.com>
2026-09-24 21:34:14 -0300

    21:34

--- main.c
+++ main.c
@@ -10,18 +10,20 @@
 int main(){
 setlocale(LC_ALL, "pt_br.UTF-8");
 
-int  n,s;
+int resultado;
 
-printf("Digite um número:\n");
-scanf("%d",&n);
+printf("Digite um número para a tabuada: ");
+resultado = scanf("%d", &n);
 
-for(int i = 1; i <= 10; i++){
-    s = i *n;
-    printf("\n%d x %d = %d", i,n,s);
+if(resultado != 1){
+    printf("Entrada inválida! Por favor, digite apenas números!");
+    return 1;
 }
 
-
-
+for(int i = 1; i <= 10; i++){
+    int resposta = n * i;
+    printf("O resultado de %d x %d é: %d\n", n, i, resposta);
+}
      return 0;
 
 
